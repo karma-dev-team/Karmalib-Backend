0e3178ebb49d941f5d2a4d9fdfd524b226da6727
pikoUsername <galymzhan.amantaj@gmail.com>
2024-10-26 23:32:56 +0500

    Initial commit

---
+++ src/main/java/com/karmalib/karmalibbackend/KarmalibBackendApplication.java
@@ -0,0 +1,13 @@
+package com.karmalib.karmalibbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmalibBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarmalibBackendApplication.class, args);
+    }
+
+}
