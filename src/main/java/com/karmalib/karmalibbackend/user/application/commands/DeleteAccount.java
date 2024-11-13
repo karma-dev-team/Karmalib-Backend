53d33b2556d7baec526f6b36d2736cf46197e9ae
pikoUsername <galymzhan.amantaj@gmail.com>
2024-11-13 13:51:15 +0500

    Small update

--- src/main/java/com/karmalib/karmalibbackend/user/application/commands/DeleteAccount.java
+++ src/main/java/com/karmalib/karmalibbackend/user/application/commands/DeleteAccount.java
@@ -2,6 +2,9 @@ package com.karmalib.karmalibbackend.user.application.commands;
 
 import com.karmalib.karmalibbackend.common.application.CommandResult;
 import com.karmalib.karmalibbackend.common.application.ICommandHandler;
+import com.karmalib.karmalibbackend.common.infrastrcuture.AccessPolicy;
+import com.karmalib.karmalibbackend.common.infrastrcuture.mailing.EmailMessage;
+import com.karmalib.karmalibbackend.common.infrastrcuture.mailing.IMailingQueue;
 import com.karmalib.karmalibbackend.user.domain.entities.UserEntity;
 import com.karmalib.karmalibbackend.user.domain.events.AccountDeleted;
 import com.karmalib.karmalibbackend.user.infrastructure.repositories.UserRepository;
@@ -13,9 +16,18 @@ import org.springframework.stereotype.Service;
 public class DeleteAccount implements ICommandHandler<DeleteAccountCommand> {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private IMailingQueue mailingQueue;
+    @Autowired
+    private AccessPolicy accessPolicy;
 
     public CommandResult handle(DeleteAccountCommand command) {
+        // удаление аккаунта происходит только после отправки эмейла
+        if (!accessPolicy.isUserSelf(command.getUserId()) || !accessPolicy.isSuperAdmin()) {
+            return CommandResult.failure("Access denied", command.getUserId());
+        }
 
+        mailingQueue.sendToQueue(EmailMessage.builder().build());
 
         return CommandResult.empty();
     }
