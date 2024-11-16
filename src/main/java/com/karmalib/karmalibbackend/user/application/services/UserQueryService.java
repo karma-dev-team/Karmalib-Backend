0b436e0f7ee79a8df6e30055e8f610a6fb1c7489
pikoUsername <galymzhan.amantaj@gmail.com>
2024-11-17 00:37:40 +0500

    Я его не вернул...

--- src/main/java/com/karmalib/karmalibbackend/user/application/services/UserQueryService.java
+++ src/main/java/com/karmalib/karmalibbackend/user/application/services/UserQueryService.java
@@ -1,12 +1,28 @@
 package com.karmalib.karmalibbackend.user.application.services;
 
-import com.karmalib.karmalibbackend.user.application.queries.GetFriends;
+import com.karmalib.karmalibbackend.user.application.queries.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserQueryService {
-    @Autowired
-    private GetFriends getFriends;
+    private final GetFriends getFriends;
+    private final GetConfidentialInfo getConfidentialInfo;
+    private final GetGroup getGroup;
+    private final GetUser getUser;
+    private final GetGroupsList getGroupsList;
+    private final GetHistoryOfProfile getHistoryOfProfile;
+    private final GetNotificationSettings getNotificationSettings;
+    private final GetUserNotifications getUserNotifications;
 
+    public UserQueryService(GetFriends getFriends, GetConfidentialInfo getConfidentialInfo, GetGroup getGroup, GetUser getUser, GetGroupsList getGroupsList, GetHistoryOfProfile getHistoryOfProfile, GetNotificationSettings getNotificationSettings, GetUserNotifications getUserNotifications) {
+        this.getFriends = getFriends;
+        this.getConfidentialInfo = getConfidentialInfo;
+        this.getGroup = getGroup;
+        this.getUser = getUser;
+        this.getGroupsList = getGroupsList;
+        this.getHistoryOfProfile = getHistoryOfProfile;
+        this.getNotificationSettings = getNotificationSettings;
+        this.getUserNotifications = getUserNotifications;
+    }
 }
